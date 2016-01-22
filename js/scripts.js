20a2e70526109d98abb054000ef4bc7da7729bed
Brad London <london.brad@gmail.com>
2016-01-22 12:08:15 -0800

    add a functioning html to show results

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,6 @@ var results = function(countTo) {
     numberArray.push(i1);
   }
 
-
   for (var i2 = 0; i2 < numberArray.length; i2++) {
     if (numberArray[i2] % 3 === 0 && numberArray[i2] % 5 === 0) {
       numberArray[i2] ="pingpong";
@@ -18,6 +17,19 @@ var results = function(countTo) {
     }
   }
   numberArray = numberArray.toString();
-  console.log(numberArray)
   return numberArray;
 }
+
+$(document).ready(function(){
+
+  $("form#pingPongGames").submit(function(event) {
+    var string = $("input#string").val();
+    var toPrint = results(string);
+
+    $("#printTo").append("<br><hr>",toPrint,"<br><hr><strong>Have Fun!</strong>");
+    $("#hide").hide();
+
+  event.preventDefault();
+  });
+
+});
