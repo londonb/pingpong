76ff760463141dc87a95c8f044b0c8cfeca53dd5
Brad London <london.brad@gmail.com>
2016-01-22 11:28:06 -0800

    detect numbers divisible by 5 and say pong

--- js/scripts.js
+++ js/scripts.js
@@ -11,11 +11,13 @@ var results = function(countTo) {
   for (var i2 = 0; i2 < numberArray.length; i2++) {
     if (numberArray[i2] % 3 === 0) {
       numberArray[i2] = "ping";
+    } else if  (numberArray[i2] % 5 === 0) {
+      numberArray[i2] = "pong";
     }
   }
   numberArray = numberArray.toString();
+  console.log(numberArray)
   return numberArray;
-console.log(numberArray)
 }
 
 
--- specs/specs.js
+++ specs/specs.js
@@ -1,6 +1,6 @@
 describe('results', function() {
-  it("will display the array as a string", function() {
-  expect(results(3)).to.equal("1,2,3");
+  it("will display every number divisible by 5 to say ping", function() {
+  expect(results(5)).to.equal("1,2,ping,4,pong");
   });
 });
 
