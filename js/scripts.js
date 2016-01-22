82eeeb28c25c42a6e371f2849d7b92d93c6c0914
Brad London <london.brad@gmail.com>
2016-01-22 11:23:32 -0800

    detect numbers divisible by 3 and say ping

--- js/scripts.js
+++ js/scripts.js
@@ -6,15 +6,22 @@ var results = function(countTo) {
   for(var i1 = 1; i1 < (result + 1); i1 += 1) {
     numberArray.push(i1);
   }
+
+
+  for (var i2 = 0; i2 < numberArray.length; i2++) {
+    if (numberArray[i2] % 3 === 0) {
+      numberArray[i2] = "ping";
+    }
+  }
   numberArray = numberArray.toString();
-  console.log(numberArray)
   return numberArray;
+console.log(numberArray)
 }
 
 
 
 
-// for (var i2 = 0); i2 < numberArray.length; i2++) {
+// for (var i2 = 0; i2 < numberArray.length; i2++) {
 //   if (numberArray[i2] % 3 === 0 && numberArray[i2] % 5 === 0) {
 //     numberArray[i2] = numberArray[i2].replace(numberArray[i2],"pingpong");
 //   }
--- specs/specs.js
+++ specs/specs.js
@@ -1,11 +1,11 @@
 describe('results', function() {
-  it("make an array for as long as the entered input", function(){
-  expect(results(3)).to.eql([1,2,3]);
+  it("will display the array as a string", function() {
+  expect(results(3)).to.equal("1,2,3");
   });
 });
 
 describe('results', function() {
-  it("will display the array as a string", function(){
-  expect(results(3)).to.equal("1,2,3");
+  it("will display every number divisible by 3 to say ping", function() {
+  expect(results(3)).to.equal("1,2,ping");
   });
 });
