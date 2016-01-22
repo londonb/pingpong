e95647475647294bc9d1695bff50f3e8d6c2d37d
Brad London <london.brad@gmail.com>
2016-01-22 14:01:18 -0800

    make the results become a list

--- js/scripts.js
+++ js/scripts.js
@@ -4,19 +4,21 @@ var results = function(countTo) {
   var result = parseInt(countTo);
 
   for(var i1 = 1; i1 < (result + 1); i1 += 1) {
+    numberArray.push("<li>")
     numberArray.push(i1);
+  
   }
 
   for (var i2 = 0; i2 < numberArray.length; i2++) {
     if (numberArray[i2] % 3 === 0 && numberArray[i2] % 5 === 0) {
-      numberArray[i2] ="pingpong";
+      numberArray[i2] ="PINGPONG!!!";
     } else if (numberArray[i2] % 3 === 0) {
       numberArray[i2] = "ping";
     } else if  (numberArray[i2] % 5 === 0) {
       numberArray[i2] = "pong";
     }
   }
-  numberArray = numberArray.toString();
+  numberArray = numberArray.join("");
   return numberArray;
 }
 
