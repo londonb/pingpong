6d54e29bfba65d854aa533c93f17d6445ba55b00
Brad London <london.brad@gmail.com>
2016-01-22 11:37:26 -0800

    fix a typo on the specs screen

--- specs/specs.js
+++ specs/specs.js
@@ -11,7 +11,7 @@ describe('results', function() {
 });
 
 describe('results', function() {
-  it("will display every number divisible by 3 and 5 to say ping", function() {
+  it("will display every number divisible by 3 and 5 to say pingpong", function() {
   expect(results(15)).to.equal("1,2,ping,4,pong,ping,7,8,ping,pong,11,ping,13,14,pingpong");
   });
 });
